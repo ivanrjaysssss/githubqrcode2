c6883addb6a09a9981117530ef35af50dae52b15
ivan rjay <ivan@evenlyten.com>
2017-03-20 12:49:34 -0700

    new item2
    descriptioin

--- www/js/index.js
+++ www/js/index.js
@@ -7,6 +7,25 @@ function init() {
 }
 function save_code(){
   //save
+
+
+  /*
+
+  var sid = document.getElementById("StudentID");
+
+    $.ajax({
+	url: 'http://localhost/qrcode_admin/save_id.php',
+	type: 'POST',
+	data: {id_number: sid},
+	success: function(response) {
+                  console.log(response);
+		alert("Saved!");
+
+	}
+});
+
+  */
+
   var modalbutton1=document.getElementById("close");
   modalbutton1.click();
   startScan();
@@ -14,20 +33,27 @@ function save_code(){
 function startScan() {
 
     cordova.plugins.barcodeScanner.scan(
+
+
+        /*function (result) {
+            var s = "Id number: <div id=StudentID>" + result.text + "</div><br/>" +
+            "Format: " + result.format + "<br/>" +
+            "Cancelled: " + result.cancelled;
+            resultDiv.innerHTML = s;
+            var modalbutton=document.getElementById("modal1");
+            modalbutton.click();
+        }*/
+
         function (result) {
             var s = "Result: " + result.text + "<br/>" +
             "Format: " + result.format + "<br/>" +
             "Cancelled: " + result.cancelled;
             resultDiv.innerHTML = s;
-
             var modalbutton=document.getElementById("modal1");
             modalbutton.click();
-
-
-        }, 
+        },
         function (error) {
             alert("Scanning failed: " + error);
         }
     );
-
 }
